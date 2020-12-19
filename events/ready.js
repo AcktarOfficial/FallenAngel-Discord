9a5ba3af770a3a216fe63cf927bcbc31bfdba55f
CodeCannibals <itsmeacktaryt@gmail.com>
2020-12-19 12:44:37 +0400

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,7 @@
+module.exports = (client) => {
+  client.user.setActivity("with the heavenpe network | -help");
+  console.log("=+=+=+=+=+=+=+=+=+=+=+=");
+  console.log("Name: FallenAngel-Discord");
+  console.log("Developer: AcktarOfficial#6930");
+  console.log("=+=+=+=+=+=+=+=+=+=+=+=");
+} 
